f17cdd74fe382005681f8b85648f203ca314c89a
Joel Alvarez <joel.ale.programmer@gmail.com>
2026-03-31 20:20:49 -0500

    feat: graficos 1, modulo para ir agregando campos y funcionales en ventanas

---
+++ DesarrolloDeSofware3/Grafico1/Grafico.java
@@ -0,0 +1,44 @@
+import  javax.swing. *;
+public class Grafico {
+  private JFrame ventana;
+  private JLabel lbl_titulo;
+  private JLabel lbl_nombre;
+  private JTextField tf_nombre;
+  private JLabel lbl_apellido;
+  private JTextField tf_apellido;
+  public static void main(String[] args) {
+    new Grafico();
+  }
+  Grafico() {
+    ventana = new JFrame("Mi Ventana");
+    ventana.setSize(500, 300);
+    ventana.setLocation(500,200);
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ventana.setLayout(null);
+
+    lbl_titulo = new JLabel("Grafico");
+    lbl_titulo.setSize(80,20);
+    lbl_titulo.setLocation(40, 20);
+    ventana.add(lbl_titulo);
+
+    lbl_nombre = new JLabel("Nombre:");
+    lbl_nombre.setSize(80,20);
+    lbl_nombre.setLocation(40, 45);
+    ventana.add(lbl_nombre);
+
+    tf_nombre = new JTextField();
+    tf_nombre.setSize(80,20);
+    tf_nombre.setLocation(125, 45);
+    ventana.add(tf_nombre);
+
+    lbl_apellido = new JLabel("Apellido:");
+    lbl_apellido.setBounds(40,70,80,20);
+    ventana.add(lbl_apellido);
+    
+    tf_apellido = new JTextField();
+    tf_apellido.setBounds(125,70,80,20);
+    ventana.add(tf_apellido);
+
+    ventana.setVisible(true);
+  }
+}
