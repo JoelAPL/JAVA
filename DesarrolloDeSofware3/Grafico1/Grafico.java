c908e5c0120cac1b1687d07d3fe91ef998b452cf
Joel Alvarez <joel.ale.programmer@gmail.com>
2026-04-07 19:11:40 -0500

    feat: actualizacion final de grafico 1

--- DesarrolloDeSofware3/Grafico1/Grafico.java
+++ DesarrolloDeSofware3/Grafico1/Grafico.java
@@ -1,44 +1,49 @@
-import  javax.swing. *;
-public class Grafico {
-  private JFrame ventana;
-  private JLabel lbl_titulo;
-  private JLabel lbl_nombre;
-  private JTextField tf_nombre;
-  private JLabel lbl_apellido;
-  private JTextField tf_apellido;
-  public static void main(String[] args) {
-    new Grafico();
-  }
-  Grafico() {
-    ventana = new JFrame("Mi Ventana");
-    ventana.setSize(500, 300);
-    ventana.setLocation(500,200);
-    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    ventana.setLayout(null);
+import javax.swing.*;
 
-    lbl_titulo = new JLabel("Grafico");
-    lbl_titulo.setSize(80,20);
-    lbl_titulo.setLocation(40, 20);
-    ventana.add(lbl_titulo);
+public class Grafico
+{
+   private JFrame ventana;
+   private JLabel lbl_titulo;
+   private JLabel lbl_nombre, lbl_apellido;
+   private JTextField tf_nombre, tf_apellido;
+   
+   public static void main(String[] args)
+   {
+      new Grafico();
+   }
 
-    lbl_nombre = new JLabel("Nombre:");
-    lbl_nombre.setSize(80,20);
-    lbl_nombre.setLocation(40, 45);
-    ventana.add(lbl_nombre);
+   Grafico()
+   {
+      ventana = new JFrame("Mi Ventana");
+      ventana.setSize(500,300);
+      ventana.setLocation(500,200);
+      ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      ventana.setLayout(null);
 
-    tf_nombre = new JTextField();
-    tf_nombre.setSize(80,20);
-    tf_nombre.setLocation(125, 45);
-    ventana.add(tf_nombre);
+      lbl_titulo = new JLabel("Grafico");
+      lbl_titulo.setSize(80,20);
+      lbl_titulo.setLocation(40,20);
+      ventana.add(lbl_titulo);
 
-    lbl_apellido = new JLabel("Apellido:");
-    lbl_apellido.setBounds(40,70,80,20);
-    ventana.add(lbl_apellido);
-    
-    tf_apellido = new JTextField();
-    tf_apellido.setBounds(125,70,80,20);
-    ventana.add(tf_apellido);
+      lbl_nombre = new JLabel("Nombre:");
+      lbl_nombre.setSize(80,20); // ancho, alto
+      lbl_nombre.setLocation(40,45);  // x, y
+      ventana.add(lbl_nombre);
 
-    ventana.setVisible(true);
-  }
+      tf_nombre = new JTextField();
+      tf_nombre.setSize(80,20);
+      tf_nombre.setLocation(125,45);
+      ventana.add(tf_nombre);
+
+      lbl_apellido = new JLabel("Apellido");
+      lbl_apellido.setBounds(40,70,80,20);  // location,size
+      ventana.add(lbl_apellido);
+
+      tf_apellido = new JTextField();
+      tf_apellido.setBounds(125,70,80,20);
+      ventana.add(tf_apellido);
+
+
+      ventana.setVisible(true);
+   }
 }
